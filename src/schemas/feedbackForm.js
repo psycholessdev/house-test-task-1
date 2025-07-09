60cc265894e0e704070fd083de90e6ee0afde70a
req-dev <uuserovich33@gmail.com>
2025-07-09 11:28:31 +1000

    Add regexp for name field

--- src/schemas/feedbackForm.js
+++ src/schemas/feedbackForm.js
@@ -3,9 +3,10 @@ import { z } from 'zod'
 export const feedbackFormSchema = z.object({
   name: z
     .string({ message: 'Имя обязательно для заполнения.' })
-    .min(2, 'Имя должно состоять как минимум из 2 символов.'),
+    .min(2, 'Имя должно состоять как минимум из 2 символов.')
+    .regex(/^[A-Za-z\u0400-\u04FF]+$/, 'Имя должно состоять только из букв русского или латинского алфавита.'),
 
-  phone: z.string({ message: 'Телефон обязателен для заполнения.' }).regex(
+  phone: z.string().regex(
     /^\+7\s?\(?\d{3}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$/,
     'Формат телефона должен соответствовать +7 (423) 123-45-67'
   ),
@@ -13,13 +14,14 @@ export const feedbackFormSchema = z.object({
   email: z
     .string()
     .email('Формат email должен соответствовать mail@example.com')
+    // email input is an empty string by default,
+    // it will not pass the email regexp
     .or(z.literal('')),
 
   message: z.string().optional(),
 }).transform(fields => {
   if (fields.email === '') {
-    // email input is an empty string by default,
-    // it will not pass the email regexp
+    // email is optional, so if it's not specified, remove the field completely
     delete fields.email
   }
 
